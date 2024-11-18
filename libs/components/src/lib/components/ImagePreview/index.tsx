a5d66dd7f77ee79e764cdcaafaabc477cf83ec19
hmquannnnn <minhquanhoang040103@gmail.com>
2024-11-18 13:56:05 +0700

    fix ui bug

--- libs/components/src/lib/components/ImagePreview/index.tsx
+++ libs/components/src/lib/components/ImagePreview/index.tsx
@@ -15,7 +15,7 @@ export const ImagePreview: React.FC<ImagePreviewProps> = ({ imageUrl, onClose })
 
 	return (
 		<div className="w-[100vw] h-[100vh] overflow-hidden fixed top-0 left-0 z-50 bg-black bg-opacity-80 flex flex-row justify-center items-center p-10">
-			<img src={imageUrl} alt="" ref={imageRef} />
+			<img src={imageUrl} alt="" ref={imageRef} className={'max-h-full max-w-full'} />
 		</div>
 	);
 };
