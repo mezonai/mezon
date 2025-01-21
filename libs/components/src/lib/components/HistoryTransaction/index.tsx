4e90abb9035e77902de9e9686dfb202a223480bb
quanace11 <quanace11@gmail.com>
2025-01-21 15:59:26 +0700

    update detail history transaction

--- libs/components/src/lib/components/HistoryTransaction/index.tsx
+++ libs/components/src/lib/components/HistoryTransaction/index.tsx
@@ -2,7 +2,7 @@ import { fetchListWalletLedger, selectWalletLedger, selectWalletLedgerCursors, u
 import { Icons } from '@mezon/ui';
 import { formatNumber } from '@mezon/utils';
 
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 interface IProps {
 	onClose: () => void;
@@ -11,7 +11,7 @@ const HistoryTransaction = ({ onClose }: IProps) => {
 	const dispatch = useAppDispatch();
 	const walletLedger = useAppSelector((state) => selectWalletLedger(state));
 	const { nextCursor, prevCursor } = useAppSelector((state) => selectWalletLedgerCursors(state));
-
+	const [selectedItem, setSelectedItem] = useState<string | null>(null);
 	useEffect(() => {
 		dispatch(fetchListWalletLedger({ cursor: undefined }));
 	}, [dispatch]);
@@ -39,6 +39,9 @@ const HistoryTransaction = ({ onClose }: IProps) => {
 		}
 		return <p className="text-green-500 font-medium">{`+${formatNumber(amount, 'vi-VN', 'VND')}`}</p>;
 	};
+	const toggleDetails = (transactionId: string) => {
+		setSelectedItem(selectedItem === transactionId ? null : transactionId);
+	};
 	return (
 		<div className="outline-none justify-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-30 focus:outline-none bg-black bg-opacity-80 dark:text-white text-black hide-scrollbar overflow-hidden">
 			<div className={`relative w-full mt-[5%] sm:h-auto rounded-lg max-w-[600px] `}>
@@ -57,15 +60,51 @@ const HistoryTransaction = ({ onClose }: IProps) => {
 						<div>
 							{walletLedger?.map((item, index) => {
 								return (
-									<div key={index} className="bg-white flex  border-b border-zinc-400 px-6 py-2 justify-between ">
-										<div className=" flex flex-col gap-2">
-											<p className="text-black">{formatDate(item.create_time ?? '')}</p>
-											<div className="text-zinc-400 flex flex-row gap-1">
-												<p className="">Transaction Code:</p>
-												<p>{item.transaction_id}</p>
+									<div key={index} className="relative ">
+										<div
+											className="bg-white flex  border-b border-zinc-400 px-6 py-2 justify-between "
+											onClick={() => toggleDetails(item.transaction_id ?? '')}
+										>
+											<div className=" flex flex-col gap-2">
+												<p className="text-black">{formatDate(item.create_time ?? '')}</p>
+												<div className="text-zinc-400 flex flex-row gap-1">
+													<p className="">Transaction Code:</p>
+													<p>{item.transaction_id}</p>
+												</div>
 											</div>
+											<div className=" flex justify-center items-center ">{renderAmount(item.value ?? 0)}</div>
 										</div>
-										<div className=" flex justify-center items-center ">{renderAmount(item.value ?? 0)}</div>
+
+										{selectedItem === item.transaction_id && (
+											<div className="bg-gray-50 overflow-hidden transition-all duration-200">
+												<div className="p-6 space-y-4 text-sm border-b border-zinc-400">
+													<div className="grid grid-cols-2 gap-x-8 gap-y-4">
+														<div>
+															<p className="text-zinc-500 mb-1">ID</p>
+															<p className="text-black font-medium">{item.id}</p>
+														</div>
+														<div>
+															<p className="text-zinc-500 mb-1">User ID</p>
+															<p className="text-black font-medium">{item.user_id}</p>
+														</div>
+														<div>
+															<p className="text-zinc-500 mb-1">Transaction ID</p>
+															<p className="text-black font-medium break-all">{item.transaction_id}</p>
+														</div>
+
+														<div>
+															<p className="text-zinc-500 mb-1">Token</p>
+															{renderAmount(item.value ?? 0)}
+														</div>
+
+														<div>
+															<p className="text-zinc-500 mb-1">Transaction Time</p>
+															<p className="text-black font-medium">{formatDate(item.create_time ?? '')}</p>
+														</div>
+													</div>
+												</div>
+											</div>
+										)}
 									</div>
 								);
 							})}
