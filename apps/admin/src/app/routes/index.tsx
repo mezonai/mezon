50ffd9335ec51e4b59fa37fc8f80bcfa00bc2562
phong.nguyen.nam <phong.nguyennam@ncc.asia>
2025-02-28 17:36:15 +0700

    toi check tiep

--- apps/admin/src/app/routes/index.tsx
+++ apps/admin/src/app/routes/index.tsx
@@ -1,21 +1,14 @@
 import loadable from '@loadable/component';
 import { selectInitialPath, useAppDispatch } from '@mezon/store';
-import { ReactFlowProvider } from '@xyflow/react';
 import { useCallback, useMemo } from 'react';
 import { useSelector } from 'react-redux';
-import { createBrowserRouter, LoaderFunctionArgs, Navigate, Outlet, RouterProvider } from 'react-router-dom';
+import { createBrowserRouter, LoaderFunctionArgs, RouterProvider } from 'react-router-dom';
 // Layouts
 import AppLayout from '../layouts/AppLayout';
-import RootLayout from '../layouts/RootLayout';
 //loader
 import { appLoader, CustomLoaderFunction } from '../loader/appLoader';
 import { authLoader } from '../loader/authLoader';
 // Pages
-import { applicationLoader } from '../loader/applicationLoader';
-import { oAuth2Loader } from '../loader/oauthLoader';
-import FlowExamples from '../pages/flowExamples';
-import Flows from '../pages/flows';
-import Flow from '../pages/flows/Flow';
 import InitialRoutes from './InititalRoutes';
 
 const Login = loadable(() => import('../pages/login'));
@@ -48,7 +41,7 @@ export const Routes = () => {
 		() =>
 			createBrowserRouter([
 				{
-					path: '',
+					path: '/developers/',
 					loader: loaderWithStore(appLoader),
 					element: <AppLayout />,
 					children: [
@@ -60,97 +53,97 @@ export const Routes = () => {
 							path: 'login',
 							element: <Login />
 						},
-						{
-							path: '',
-							loader: loaderWithStore(authLoader),
-							element: <RootLayout />,
-							children: [
-								{
-									path: '',
-									element: <InitialRoutes />
-								},
-								{
-									path: 'applications',
-									element: <ApplicationsPage />
-								},
-								{
-									path: 'applications/:applicationId',
-									element: (
-										<div>
-											<Outlet />
-										</div>
-									),
-									loader: loaderWithStore(applicationLoader),
-									children: [
-										{
-											path: '*',
-											element: <Navigate to="information" />
-										},
-										{
-											path: '',
-											element: <Navigate to="information" />
-										},
-										{
-											path: 'information',
-											element: <GeneralInformation />
-										},
-										{
-											path: 'installation',
-											element: <Installation />
-										},
-										{
-											path: 'oauth2',
-											loader: loaderWithStore(oAuth2Loader),
-											element: <OAuth2 />
-										},
-										{
-											path: 'flow',
-											element: <Flows />
-										},
-										{
-											path: 'add-flow',
-											element: (
-												<ReactFlowProvider>
-													<Flow />
-												</ReactFlowProvider>
-											)
-										},
-										{
-											path: 'flow/:flowId',
-											element: (
-												<ReactFlowProvider>
-													<Flow />
-												</ReactFlowProvider>
-											)
-										},
-										{
-											path: 'flow-examples',
-											element: <FlowExamples />
-										},
-										{
-											path: 'use-flow-example/:exampleFlowId',
-											element: (
-												<ReactFlowProvider>
-													<Flow />
-												</ReactFlowProvider>
-											)
-										}
-									]
-								},
-								{
-									path: 'teams',
-									element: <TeamsPage />
-								},
-								{
-									path: 'embeds',
-									element: <EmbedsPage />
-								},
-								{
-									path: 'docs',
-									element: <DocsPage />
-								}
-							]
-						},
+						// {
+						// 	path: '',
+						// 	loader: loaderWithStore(authLoader),
+						// 	element: <RootLayout />,
+						// 	children: [
+						// 		{
+						// 			path: '',
+						// 			element: <InitialRoutes />
+						// 		},
+						// 		{
+						// 			path: 'applications',
+						// 			element: <ApplicationsPage />
+						// 		},
+						// 		{
+						// 			path: 'applications/:applicationId',
+						// 			element: (
+						// 				<div>
+						// 					<Outlet />
+						// 				</div>
+						// 			),
+						// 			loader: loaderWithStore(applicationLoader),
+						// 			children: [
+						// 				{
+						// 					path: '*',
+						// 					element: <Navigate to="information" />
+						// 				},
+						// 				{
+						// 					path: '',
+						// 					element: <Navigate to="information" />
+						// 				},
+						// 				{
+						// 					path: 'information',
+						// 					element: <GeneralInformation />
+						// 				},
+						// 				{
+						// 					path: 'installation',
+						// 					element: <Installation />
+						// 				},
+						// 				{
+						// 					path: 'oauth2',
+						// 					loader: loaderWithStore(oAuth2Loader),
+						// 					element: <OAuth2 />
+						// 				},
+						// 				{
+						// 					path: 'flow',
+						// 					element: <Flows />
+						// 				},
+						// 				{
+						// 					path: 'add-flow',
+						// 					element: (
+						// 						<ReactFlowProvider>
+						// 							<Flow />
+						// 						</ReactFlowProvider>
+						// 					)
+						// 				},
+						// 				{
+						// 					path: 'flow/:flowId',
+						// 					element: (
+						// 						<ReactFlowProvider>
+						// 							<Flow />
+						// 						</ReactFlowProvider>
+						// 					)
+						// 				},
+						// 				{
+						// 					path: 'flow-examples',
+						// 					element: <FlowExamples />
+						// 				},
+						// 				{
+						// 					path: 'use-flow-example/:exampleFlowId',
+						// 					element: (
+						// 						<ReactFlowProvider>
+						// 							<Flow />
+						// 						</ReactFlowProvider>
+						// 					)
+						// 				}
+						// 			]
+						// 		},
+						// 		{
+						// 			path: 'teams',
+						// 			element: <TeamsPage />
+						// 		},
+						// 		{
+						// 			path: 'embeds',
+						// 			element: <EmbedsPage />
+						// 		},
+						// 		{
+						// 			path: 'docs',
+						// 			element: <DocsPage />
+						// 		}
+						// 	]
+						// },
 						{
 							path: 'install/:applicationId',
 							loader: loaderWithStore(authLoader),
