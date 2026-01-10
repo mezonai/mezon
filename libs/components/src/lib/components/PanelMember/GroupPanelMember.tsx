bcfff1f1f2e22accee4835164dbc989289b27f5c
kind6688 <thin.tranhoang@ncc.asia>
2026-01-10 16:45:56 +0700

    update logo and border

--- libs/components/src/lib/components/PanelMember/GroupPanelMember.tsx
+++ libs/components/src/lib/components/PanelMember/GroupPanelMember.tsx
@@ -3,7 +3,11 @@ type GroupPanelMemberProps = {
 };
 
 const GroupPanelMember = ({ children }: GroupPanelMemberProps) => {
-	return <div className="flex flex-col pb-1 mb-1 border-b-[0.08px] border-b-[#6A6A6A] last:border-b-0 last:mb-0 last:pb-0">{children}</div>;
+	return (
+		<div className="flex flex-col pb-1 mb-1 border-b-[0.08px] border-b-[#6A6A6A] last:border-b-0 last:mb-0 last:pb-0 no-divider-last ">
+			{children}
+		</div>
+	);
 };
 
 export default GroupPanelMember;
