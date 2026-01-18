32589db9cc857865711015b4f090062702d2f360
Nhan Nguyen <nguyentran@ncc.asia>
2026-01-18 20:11:52 +0700

    add map

--- libs/utils/src/lib/utils/message.ts
+++ libs/utils/src/lib/utils/message.ts
@@ -1,6 +1,7 @@
 import type { ChannelMessage } from 'mezon-js';
 import { safeJSONParse } from 'mezon-js';
-import type { IMessageWithUser } from '../types';
+import type { ApiMessageMention, ApiMessageRef } from 'mezon-js/api.gen';
+import type { IMessageMention, IMessageRef, IMessageWithUser } from '../types';
 import { EMessageCode } from '../types';
 
 interface MessagesEntity extends IMessageWithUser {
@@ -14,20 +15,43 @@ interface MessagesEntity extends IMessageWithUser {
 
 const NX_CHAT_APP_ANNONYMOUS_USER_ID = process.env.NX_CHAT_APP_ANNONYMOUS_USER_ID || 'anonymous';
 
+const mapMention = (mention: ApiMessageMention): IMessageMention => ({
+	...mention,
+	id: String(mention.id),
+	user_id: String(mention.user_id),
+	role_id: String(mention.role_id),
+	channel_id: String(mention.channel_id)
+});
+
+const mapMessageRef = (ref: ApiMessageRef): IMessageRef => ({
+	...ref,
+	message_id: String(ref.message_id),
+	message_ref_id: String(ref.message_ref_id),
+	message_sender_id: String(ref.message_sender_id),
+	channel_id: ref.channel_id
+});
+
 const mapMessageChannelToEntity = (channelMess: ChannelMessage, lastSeenId?: string): IMessageWithUser => {
 	const creationTime = new Date(channelMess.create_time_seconds || '');
-	const isAnonymous = channelMess?.sender_id === NX_CHAT_APP_ANNONYMOUS_USER_ID;
+	const isAnonymous = String(channelMess?.sender_id) === NX_CHAT_APP_ANNONYMOUS_USER_ID;
 	return {
 		...channelMess,
+		mentions: channelMess.mentions?.map(mapMention) || [],
+		references: channelMess.references?.map(mapMessageRef) || [],
+		topic_id: String(channelMess.topic_id),
+		message_id: String(channelMess.message_id),
+		sender_id: String(channelMess.sender_id),
+		clan_id: String(channelMess.clan_id),
+		channel_id: String(channelMess.channel_id),
 		isFirst: channelMess.code === EMessageCode.FIRST_MESSAGE,
 		creationTime,
-		id: channelMess.id || channelMess.message_id || '',
+		id: String(channelMess.id) || String(channelMess.message_id) || '',
 		date: new Date().toLocaleString(),
 		isAnonymous,
 		user: {
 			name: channelMess.username || '',
 			username: channelMess.username || '',
-			id: channelMess.sender_id || ''
+			id: String(channelMess.sender_id) || ''
 		},
 		lastSeen: lastSeenId === (channelMess.id || channelMess.message_id),
 		create_time_seconds: channelMess.create_time_seconds || creationTime.getTime() / 1000
