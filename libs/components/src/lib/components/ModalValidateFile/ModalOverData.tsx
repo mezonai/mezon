1715ca636a25843bbb23fa1b68d6500452156ffe
Nguyen Luc <nguyentatluc1442003@gmail.com>
2026-01-22 13:36:31 +0700

    fix UI noti upload voice sticker

--- libs/components/src/lib/components/ModalValidateFile/ModalOverData.tsx
+++ libs/components/src/lib/components/ModalValidateFile/ModalOverData.tsx
@@ -4,8 +4,19 @@ export const ModalOverData = ({ onClose, open, size = ELimitSize.MB }: { onClose
 	return <ModalValidateFile onClose={onClose} open={open} title={'Your files are too powerful'} content={`Max file size is ${size}, please!`} />;
 };
 
-export const ModalErrorTypeUpload = ({ onClose, open, size = ELimitSize.MB }: { onClose: () => void; open?: boolean; size?: string }) => {
+export const ModalErrorTypeUpload = ({ onClose, open }: { onClose: () => void; open?: boolean; size?: string }) => {
 	return (
 		<ModalValidateFile onClose={onClose} open={open} title={'Only image files are allowed'} content={`Just upload type file images, please!`} />
+	);
+};
+
+export const ModalErrorTypeUploadVoice = ({ onClose, open }: { onClose: () => void; open?: boolean; size?: string }) => {
+	return (
+		<ModalValidateFile
+			onClose={onClose}
+			open={open}
+			title={'Only voice sticker files are allowed'}
+			content={`Just upload type file voice sticker, please!`}
+		/>
 	);
 };
