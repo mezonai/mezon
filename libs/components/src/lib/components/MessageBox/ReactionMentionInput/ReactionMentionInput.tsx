cc71bb726fa0d9c8dd91a93b2186efade4195c7f
huylexuan44 <lexuanhuy4497@gmail.com>
2026-01-29 14:42:52 +0700

    feat(threads): fix active archived thread logic and update mezon-js deps

--- libs/components/src/lib/components/MessageBox/ReactionMentionInput/ReactionMentionInput.tsx
+++ libs/components/src/lib/components/MessageBox/ReactionMentionInput/ReactionMentionInput.tsx
@@ -254,13 +254,14 @@ export const MentionReactBase = memo((props: MentionReactBaseProps): ReactElemen
 
 			const currentTime = Math.floor(Date.now() / 1000);
 			const lastMessageTimestamp = channel.last_sent_message?.timestamp_seconds;
+
 			const isArchived = lastMessageTimestamp && currentTime - Number(lastMessageTimestamp) > THREAD_ARCHIVE_DURATION_SECONDS;
 
 			const store = getStore();
 			const userIds = selectMemberIdsByChannelId(store.getState(), channel.id as string);
 			const needsJoin = !userProfile?.user?.id ? false : !userIds?.includes(userProfile?.user?.id);
 
-			if (isArchived) {
+			if (isArchived || channel.active === 0) {
 				await dispatch(
 					threadsActions.writeActiveArchivedThread({
 						clanId: channel.clan_id ?? '',
