7da66af416cf6a14b8d032bd826a42ef84d20c2b
hoangtranlehuy <hoang.tranlehuy@ncc.asia>
2026-02-10 13:17:08 +0700

    fix comment

--- apps/mobile/src/app/screens/home/homedrawer/components/ChannelVoice/HeaderRoomView/AgentControl.tsx
+++ apps/mobile/src/app/screens/home/homedrawer/components/ChannelVoice/HeaderRoomView/AgentControl.tsx
@@ -2,6 +2,7 @@ import { baseColor, size, useTheme } from '@mezon/mobile-ui';
 import { handleAddAgentToVoice, handleKichAgentFromVoice, selectVoiceInfo, useAppDispatch } from '@mezon/store-mobile';
 import React, { memo, useCallback, useState } from 'react';
 import { TouchableOpacity } from 'react-native';
+import { Chase } from 'react-native-animated-spinkit';
 import { useSelector } from 'react-redux';
 import MezonIconCDN from '../../../../../../componentUI/MezonIconCDN';
 import { IconCDN } from '../../../../../../constants/icon_cdn';
@@ -12,24 +13,36 @@ const SendVoiceSound = memo(() => {
 	const styles = style(themeValue);
 	const currentVoice = useSelector(selectVoiceInfo);
 	const [onAgent, setOnAgent] = useState(false);
+	const [loading, setLoading] = useState(false);
 	const dispatch = useAppDispatch();
 
 	const handleAddAgent = useCallback(() => {
-		if (!currentVoice) {
-			return;
-		}
-		setOnAgent(!onAgent);
+		try {
+			if (!currentVoice || loading) {
+				return;
+			}
+			setOnAgent(!onAgent);
+			setLoading(true);
 
-		if (!onAgent) {
-			dispatch(handleAddAgentToVoice({ channel_id: currentVoice?.channelId, room_name: currentVoice?.roomId || '' }));
-		} else {
-			dispatch(handleKichAgentFromVoice({ channel_id: currentVoice?.channelId, room_name: currentVoice?.roomId || '' }));
+			if (!onAgent) {
+				dispatch(handleAddAgentToVoice({ channel_id: currentVoice?.channelId, room_name: currentVoice?.roomId || '' }));
+			} else {
+				dispatch(handleKichAgentFromVoice({ channel_id: currentVoice?.channelId, room_name: currentVoice?.roomId || '' }));
+			}
+		} catch (error) {
+			console.error(error);
+		} finally {
+			setLoading(false);
 		}
-	}, [currentVoice, dispatch, onAgent]);
+	}, [currentVoice, dispatch, onAgent, loading]);
 
 	return (
-		<TouchableOpacity onPress={handleAddAgent} style={[styles.buttonCircle, onAgent && styles.buttonAgentActive]}>
-			<MezonIconCDN icon={IconCDN.agentIcon} height={size.s_20} width={size.s_20} color={onAgent ? baseColor.white : themeValue.white} />
+		<TouchableOpacity disabled={loading} onPress={handleAddAgent} style={[styles.buttonCircle, onAgent && styles.buttonAgentActive]}>
+			{loading ? (
+				<Chase size={size.s_20} color={onAgent ? baseColor.white : themeValue.white} />
+			) : (
+				<MezonIconCDN icon={IconCDN.agentIcon} height={size.s_20} width={size.s_20} color={onAgent ? baseColor.white : themeValue.white} />
+			)}
 		</TouchableOpacity>
 	);
 });
