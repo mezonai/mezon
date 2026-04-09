081fe3ccfb0d69abffb9a0a57e175a0299606a18
Nguyen Luc <nguyentatluc1442003@gmail.com>
2026-04-09 10:02:47 +0700

    add french language

--- libs/components/src/lib/components/SettingLanguage/index.tsx
+++ libs/components/src/lib/components/SettingLanguage/index.tsx
@@ -53,6 +53,11 @@ export const SettingLanguage = ({ menuIsOpen }: ISettingLanguageProps) => {
 				title: `${t('setting:language.japanese')}`,
 				value: 'jpn',
 				contributedBy: 'robits'
+			},
+			{
+				title: `${t('setting:language.french')}`,
+				value: 'fr',
+				contributedBy: 'robits'
 			}
 		];
 	}, [t]);
