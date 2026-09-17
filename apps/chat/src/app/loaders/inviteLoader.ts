c3c35e9ed15c7554be4237182aab201234e15ce4
sadohpv <sigaldphp@gmail.com>
2026-09-17 10:08:34 +0700

    fix alert on linux

--- apps/chat/src/app/loaders/inviteLoader.ts
+++ apps/chat/src/app/loaders/inviteLoader.ts
@@ -1,15 +1,17 @@
 import { inviteActions } from '@mezon/store';
-import { ShouldRevalidateFunction } from 'react-router-dom';
-import { CustomLoaderFunction } from './appLoader';
+import type { ShouldRevalidateFunction } from 'react-router-dom';
+import type { CustomLoaderFunction } from './appLoader';
 
 export const inviteLoader: CustomLoaderFunction = async ({ params, dispatch }) => {
 	const { inviteId } = params;
+
 	if (!inviteId) {
 		throw new Error('inviteId ID null');
 	}
-	dispatch(inviteActions.getLinkInvite({ inviteId: inviteId }));
+	dispatch(inviteActions.getLinkInvite({ inviteId }));
 	// Handle auto open deeplink when load on mobile
-	if (inviteId) {
+	const isLinux = /Linux/i.test(navigator.userAgent);
+	if (inviteId && !isLinux) {
 		try {
 			window.location.href = `mezon.ai://invite/${inviteId}`;
 		} catch (e) {
